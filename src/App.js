a13d3f259d16ce118132d4e1849f4910b3669800
jamesgelok <jdgelok@gmail.com>
2018-02-27 17:33:11 -0500

    Made it look pretty nice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
+import logo from './img/logo.png';
+import exampleSentanceImage from './img/example-sentance.png';
+import calculatedPicture from './img/calculated-picture.png';
 import './App.css';
+
+const exampleSentance = "This sentance was stored in a variable within App.js.";
 
 class App extends Component {
   render() {
@@ -8,8 +12,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, World! <br /> Computer Science Club</h1>
+          <h1 className="App-title">Hello, World!<br />Computer Science Club</h1>
         </header>
+
+        <p>I am a calculated {1 + 2 + 3 + 4 + 5 + 6} years old.</p>
+        <img src={calculatedPicture} alt="calculated value" style={{height: '64px'}}/>
+
+        <p>{exampleSentance} Neat!</p>
+        <img src={exampleSentanceImage} alt="example sentance" style={{height: '64px'}}/>
+
       </div>
     );
   }
