dfcd0559a0db168dda6038d4955a6a3b475928db
jamesgelok <jdgelok@gmail.com>
2018-02-27 11:14:49 -0500

    Super Mega Big Initial Change
    Used create-react-app to make us something that wasn't ridiculous.
    "literally did everything" -- josh

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello, World! <br /> Computer Science Club</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
