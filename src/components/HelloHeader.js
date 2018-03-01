336e9073d926e831d6bfd22bd558eed8b3f50b4b
joshuasimmons33 <34275991+joshuasimmons33@users.noreply.github.com>
2018-02-28 20:52:13 -0500

    Removed unecessary calls

--- src/components/HelloHeader.js
+++ src/components/HelloHeader.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../css/images/logo.png';
-import calculatedPicture from '../css/images/calculated-picture.png';
-import exampleSentanceImage from '../css/images/example-sentance.png';
 
-const exampleSentance = "This sentance was stored in a variable within App.js.";
 
 class HelloHeader extends Component {
   render() {
