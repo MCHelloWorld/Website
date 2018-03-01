a8c8d29505526e69ab1482293e1a7dff2aff79c7
jamesgelok <jdgelok@gmail.com>
2018-02-28 19:48:41 -0500

    Updated HelloHeader

---
+++ src/components/HelloHeader.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../css/images/logo.png';
+import calculatedPicture from '../css/images/calculated-picture.png';
+import exampleSentanceImage from '../css/images/example-sentance.png';
+
+const exampleSentance = "This sentance was stored in a variable within App.js.";
+
+class HelloHeader extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Hello, World!<br />Computer Science Club</h1>
+        <Link to="/secret" className="App-title">SECRET PAGE</Link>
+      </header>
+    );
+  }
+}
+
+export default HelloHeader;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import HelloHeader from './HelloHeader.js';
 import logo from '../css/images/logo.png';
 import calculatedPicture from '../css/images/calculated-picture.png';
 import exampleSentanceImage from '../css/images/example-sentance.png';
@@ -10,12 +11,7 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, World!<br />Computer Science Club</h1>
-          <Link to="/secret" className="App-title">SECRET PAGE</Link>
-        </header>
-
+        <HelloHeader/>
         <p>I am a calculated {1 + 2 + 3 + 4 + 5 + 6} years old.</p>
         <img src={calculatedPicture} alt="calculated value" style={{height: '64px'}}/>
 
