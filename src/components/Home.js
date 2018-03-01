595823cecc420ccc814255e0fc10e92ad968d258
joshuasimmons33 <34275991+joshuasimmons33@users.noreply.github.com>
2018-02-28 21:41:13 -0500

    More formatting changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,15 +11,21 @@ class Home extends Component {
     return (
       <div className="App">
         <HelloHeader/>
-        <p>I am a calculated {1 + 2 + 3 + 4 + 5 + 6} years old.</p>
+        <br />
         <Link to="/Login" className="App-title">Login</Link>&nbsp;&nbsp;&nbsp;
         <Link to="/Registration" className="App-title">Registration</Link><br/>
+        <p>I am a calculated {1 + 2 + 3 + 4 + 5 + 6} years old.</p>
         <img src={calculatedPicture} alt="calculated value" style={{height: '64px'}}/>
 
         <p>{exampleSentance} Neat!</p>
 
         <img src={exampleSentanceImage} alt="example sentance" style={{height: '64px'}}/>
         <br/>
+
+        <h1>Events</h1>
+        <p>Future events will go here!</p>    
+        <h1>OpenKattis Leaderboard</h1>
+        <p>OpenKattis Leaderboard will appear here!</p>
 
       </div>
     );
