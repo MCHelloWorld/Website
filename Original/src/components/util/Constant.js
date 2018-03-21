a76cb54f3a10f0a778d77cab1e0b0334a024ab27
jamesgelok <jdgelok@gmail.com>
2018-03-21 18:19:46 -0400

    deleted original

--- Original/src/components/util/Constant.js
+++
@@ -1,8 +0,0 @@
-const Constant = {
-  HOME_PATH: "/",
-  LOGIN_PATH: "/login/Login",
-  REGISTRATION_PATH: "/Registration",
-  PROFILE_PAGE_PATH: "/ProfilePage",
-  SECRET_PATH: "/Secret"
-}
-export default Constant;
