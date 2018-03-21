fefbd7c0b753af96c9a28d5b34235bc2bc00ae6b
jamesgelok <jdgelok@gmail.com>
2018-03-21 19:34:50 -0400

    removed more homepage lint

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import HelloHeader from './HelloHeader.js';
-import calculatedPicture from '../css/images/calculated-picture.png';
-import exampleSentanceImage from '../css/images/example-sentance.png';
 import Constant from './util/Constant.js';
 import { Link } from 'react-router-dom'
-
-const exampleSentance = "This sentance was stored in a variable within App.js.";
 
 class Home extends Component {
   render() {
