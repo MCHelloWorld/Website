1493075f25936c10c57a7ec0151c76c6f2fe9784
jamesgelok <jdgelok@gmail.com>
2018-03-21 18:59:07 -0400

    Registration path changed in router

--- test_login_with_router/frontend/src/index.js
+++ test_login_with_router/frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './user/index.css';
+import Registration from './user/Register';
 import User from './user/User';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -16,6 +17,7 @@ const Root = () => {
       <Switch>
           <Route exact path={Constant.HOME_PATH} component={Home} title="Home"/>
           <Route exact path='/User' component={User} title="User" />
+          <Route exact path={Constant.REGISTRATION_PATH} component={Registration} title="Registration" />
           <Route exact path={Constant.SECRET_PATH} component={Secret} title="Secret"/>
           <Route exact path="/LoginSuccess" component={UploadScreen} />
           {/*<Route exact path='*' component={NotFound} />*/}
