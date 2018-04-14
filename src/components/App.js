4d9fc5e46f753d675cdffeda205e28111e324b94
jamesgelok <jdgelok@gmail.com>
2018-04-14 10:21:10 -0400

    reformatted app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Home from './Home.js'
-import Login from './Login.js'
-import Secret from './Secret.js'
-import Special from './Special.js'
-import NotFound from './NotFound.js'
-import User from '../user/User.js'
-import Constant from '../utils/Constant.js'
+import Home from "./Home.js";
+import Login from "./Login.js";
+import Secret from "./Secret.js";
+import Special from "./Special.js";
+import NotFound from "./NotFound.js";
+import User from "../user/User.js";
+import Constant from "../utils/Constant.js";
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.setAppState = this.props.setAppState.bind(this)
-    this.appState = this.props.appState
-    console.log('State from App.js:')
-    console.log(this.state)
+    super(props);
+    this.setAppState = this.props.setAppState.bind(this);
+    this.appState = this.props.appState;
+    console.log("State from App.js:");
+    console.log(this.state);
   }
 
   render() {
@@ -42,8 +42,8 @@ class App extends Component {
           <Route exact path="*" render={() => <NotFound {...this.props} />} />
         </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default App
+export default App;
