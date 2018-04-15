be3e009fb14ec2632f8ca5ffcac989ae4801da33
jamesgelok <jdgelok@gmail.com>
2018-04-15 13:51:11 -0400

    Ran Prettier on Server Folder

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,91 +1,102 @@
-var mysql = require('mysql');
-var bcrypt = require('bcrypt');
-var connection = require('./connection');
+var mysql = require("mysql");
+var bcrypt = require("bcrypt");
+var connection = require("./connection");
 
-exports.edit = function(req, res){
+exports.edit = function(req, res) {
   var today = new Date();
   //bcrypt.hash(req.body.password, 5, function(err, bcryptedPassword) {
-  var payload={
-    "first_name":req.body.first_name,
-    "last_name":req.body.last_name,
-    "bio":req.body.bio,
-    "modified":today,
-    "email":req.body.email
-  }
+  var payload = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    bio: req.body.bio,
+    modified: today,
+    email: req.body.email
+  };
 
-  var str = 'UPDATE user SET ?';
-  if (req.body.hasOwnProperty('password')) {
+  var str = "UPDATE user SET ?";
+  if (req.body.hasOwnProperty("password")) {
     var salt = bcrypt.genSaltSync(5);
     var hashed = bcrypt.hashSync(req.body.password, salt);
     payload.hash = hashed;
   }
 
-  connection.query('SELECT * FROM user WHERE email = ?', [payload.email], function(error, results, fields) {
-    console.log(results.length);
-    if(results.length > 0){
-      connection.query(str, payload, function (error, results, fields) {
-        if(error) {
-          console.log(error);
-        } else {
-          res.send({
-            "code":100,
-            "success":"Success!",
-            "returned":payload
-          })
-        }
-      })
+  connection.query(
+    "SELECT * FROM user WHERE email = ?",
+    [payload.email],
+    function(error, results, fields) {
+      console.log(results.length);
+      if (results.length > 0) {
+        connection.query(str, payload, function(error, results, fields) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send({
+              code: 100,
+              success: "Success!",
+              returned: payload
+            });
+          }
+        });
+      }
     }
-  })
-}
+  );
+};
 
 exports.upload = function(req, res) {
   var today = new Date();
   var payload = {
-    "file":req.body.file,
-    "url":req.body.imagePreviewUrl,
-    "email":req.body.email
-  }
+    file: req.body.file,
+    url: req.body.imagePreviewUrl,
+    email: req.body.email
+  };
 
-  connection.query('INSERT INTO user SET profile_picture = ?, url = ? WHERE email = ?',
-                    [payload.file, payload.url, payload.email], function(error, results, fields) {
-    console.log(results.length);
-    if(error) {
-      console.log(error);
-    } else {
-      res.send({
-        "code":300,
-        "success":"Success!"
-      })
+  connection.query(
+    "INSERT INTO user SET profile_picture = ?, url = ? WHERE email = ?",
+    [payload.file, payload.url, payload.email],
+    function(error, results, fields) {
+      console.log(results.length);
+      if (error) {
+        console.log(error);
+      } else {
+        res.send({
+          code: 300,
+          success: "Success!"
+        });
+      }
     }
-  })
-}
-exports.register = function(req,res){
+  );
+};
+exports.register = function(req, res) {
   // console.log("req",req.body);
   var today = new Date();
-  bcrypt.hash(req.body.password, 5, function(err, bcryptedPassword){
-    var users={
-      "first_name":req.body.first_name,
-      "last_name":req.body.last_name,
-      "email":req.body.email,
-      "username":req.body.email.substring(0,6),
-      "hash":bcryptedPassword,
-      "created":today,
-      "modified":today
-    }
-    connection.query('INSERT INTO user SET ?',users, function (error, results, fields) {
-    if (error) {
-      console.log("error ocurred",error);
-      res.send({
-        "code":400,
-        "failed":"error ocurred"
-      })
-    } else {
-      console.log('The solution is: ', results);
-      res.send({
-        "code":200,
-        "success":"user registered sucessfully"
-          });
-    }
+  bcrypt.hash(req.body.password, 5, function(err, bcryptedPassword) {
+    var users = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      username: req.body.email.substring(0, 6),
+      hash: bcryptedPassword,
+      created: today,
+      modified: today
+    };
+    connection.query("INSERT INTO user SET ?", users, function(
+      error,
+      results,
+      fields
+    ) {
+      if (error) {
+        console.log("error ocurred", error);
+        res.send({
+          code: 400,
+          failed: "error ocurred"
+        });
+      } else {
+        console.log("The solution is: ", results);
+        res.send({
+          code: 200,
+          success: "user registered sucessfully"
+        });
+      }
     });
   });
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,49 +1,55 @@
-const express    = require("express");
-const login = require('./routes/loginroutes');
-const special = require('./routes/special');
-const bodyParser = require('body-parser');
-const user = require('./routes/user');
-const session = require('cookie-session');
+const express = require("express");
+const login = require("./routes/loginroutes");
+const special = require("./routes/special");
+const bodyParser = require("body-parser");
+const user = require("./routes/user");
+const session = require("cookie-session");
 const functions = require("./utils/functions");
 const CryptoJS = require("crypto-js");
 const CryptoKey = "feijwqfhaivrqfnaobg3q4ngui9p";
 
-
-var rp = require('request-promise');
+var rp = require("request-promise");
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // QUESTION: What is body parser for? ~James
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use(session({
-  name: 'server-session-cookie-hwcsc',
-  secret: "E3cHjHM349sGXRj4KFPsW3dd",
-  resave: false,
-  saveUninitialized: true,
-  maxAge: 6000
-}));
+app.use(
+  session({
+    name: "server-session-cookie-hwcsc",
+    secret: "E3cHjHM349sGXRj4KFPsW3dd",
+    resave: false,
+    saveUninitialized: true,
+    maxAge: 6000
+  })
+);
 
 var router = express.Router();
 
 //route to handle user registration
-router.post('/register', login.register);
-router.post('/login', login.login);
-router.post('/special', special.special); // for testing and debugging
-router.post('/user/edit', user.edit);
-router.post('/user/status', function(req,res) {
+router.post("/register", login.register);
+router.post("/login", login.login);
+router.post("/special", special.special); // for testing and debugging
+router.post("/user/edit", user.edit);
+router.post("/user/status", function(req, res) {
   if (!req.session.user) {
-    return res.status(401).send();    /* If user is not logged in, then send Unauthorized 401 error */
+    return res
+      .status(401)
+      .send(); /* If user is not logged in, then send Unauthorized 401 error */
   }
 
   return res.status(200).send("Welcome to Super Secret API");
 });
-router.post('/session', function(req,res) {
+router.post("/session", function(req, res) {
   req.session.user = req.body.user;
   req.session.pass = req.body.user;
 });
@@ -53,30 +59,33 @@ router.post('/session', function(req,res) {
    | ever localhost:5000 is curl'd (as in: curl localhost:5000 )
    ========================================================================== */
 
-app.get('/', function initViewsCount(req, res, next) {
+app.get("/", function initViewsCount(req, res, next) {
   console.log(req.sessionOptions.maxAge);
-  if (typeof req.session.views === 'undefined') {
+  if (typeof req.session.views === "undefined") {
     req.session.views = 0;
-    return res.end('Welcome to the file session demo. Refresh page!');
+    return res.end("Welcome to the file session demo. Refresh page!");
   }
   return next();
 });
-app.get('/', function incrementViewsCount(req, res, next) {
-  console.assert(typeof req.session.views === 'number',
-  'missing views count in the session', req.session);
+app.get("/", function incrementViewsCount(req, res, next) {
+  console.assert(
+    typeof req.session.views === "number",
+    "missing views count in the session",
+    req.session
+  );
   req.session.views++;
   return next();
 });
 app.use(function printSession(req, res, next) {
-  console.log('req.session: ', req.session);
+  console.log("req.session: ", req.session);
   return next();
 });
-app.get('/', function sendPageWithCounter(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<p>views: ' + req.session.views + '</p>\n');
-  res.write('<p>expires in: ' + (req.sessionOptions.maxAge / 1000) + 's</p>')
+app.get("/", function sendPageWithCounter(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<p>views: " + req.session.views + "</p>\n");
+  res.write("<p>expires in: " + req.sessionOptions.maxAge / 1000 + "s</p>");
   res.end();
 });
 
-app.use('/api', router);
+app.use("/api", router);
 app.listen(5000);
