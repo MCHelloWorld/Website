28d292114a1f887a4ffbe11c38b439c25a72ad5b
Josh <joshua.simmons33@gmail.com>
2018-04-25 23:46:31 -0400

    finalized sponsor and FAQ pages

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,9 +3,12 @@ import HelloHeader from "./HelloHeader.js";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
+
+//This page is an FAQ page, where users can view information about
+// the club.
 const style = {
   margin: 15
-  };
+};
 
 class FAQ extends Component{
   constructor(props) {
@@ -25,7 +28,7 @@ class FAQ extends Component{
         style={style}
         href="/"
       />
-      <div className = "FAQ">
+      <div className = "curved">
       <h2>What is Hello, World?</h2>
       <p> Hello, World is Messiah College's computer science club,
         where members get to learn about all kinds of things
@@ -33,30 +36,37 @@ class FAQ extends Component{
       </p>
       </div>
       <br />
-      <div className = "FAQ">
+      <div className = "curved">
       <h2>When/where does the club meet?</h2>
       <p>The club meets in Frey 151, the Computer Science Lounge.
         Typically, meetings are held on Thursdays at 7pm, and run for about an hour.</p>
       </div>
       <br/>
-      <div className = "FAQ">
+      <div className = "curved">
       <h2>What are the meetings about?</h2>
       <p>There are meetings about all kinds of things. Anywhere from learning the ins and outs of GitHub,
         to making your own JavaScript applets!</p>
       </div>
       <br/>
-      <div className = "FAQ">
+      <div className = "curved">
       <h2>Do I have to be a Comp Sci major to join?</h2>
       <p>Nope! The meetings are for anyone and everyone!</p>
       </div>
       <br/>
-      <div className = "FAQ">
+      <div className = "curved">
       <h2>Can I lead a meeting, or suggest a topic for a future one?</h2>
       <p>Absolutely! Contact the club leadership for details. The current
         club president is James Gelok (jg1483@messiah.edu).</p>
       </div>
       <br/>
-      <div className = "FAQ">
+      <div className = "curved">
+      <h2>What other kind of events are there?</h2>
+      <p>Besides our engaging, beginner-friendly workshops, we also
+      organize group trips to hack-a-thons, or programming competitions. We try
+      to organize one trip per month, but sometimes there's more!</p>
+      </div>
+      <br/>
+      <div className = "curved">
       <h2>Can one of you fix my computer? It's been acting kind of slow...</h2>
       <p>Have you tried turning it off and on again?<br/>
         (But seriously, you should take it to CSS. They'll be happy to help!)</p>
--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -3,6 +3,12 @@ import HelloHeader from "./HelloHeader.js";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
+//This is a sponsor page, where users can view current alumni/additional
+//sponsors.
+const style = {
+  margin: 15
+};
+
 class Sponsor extends Component {
   constructor(props) {
     super(props);
@@ -14,7 +20,24 @@ class Sponsor extends Component {
       <MuiThemeProvider>
       <div className="App">
       <HelloHeader/>
+      <h1 style={{'font-size': '50px'}}>Sponsors and Supporters</h1>
+      <RaisedButton
+        label="Home"
+        primary={true}
+        style={style}
+        href="/"
+      />
+      <div className="curved">
+      <h2>Sponsors</h2>
+      <p>This is where future sponsors will appear, with photos
+      and other relevant information.</p>
+      <br/>
+      <h2>Contact Us!</h2>
+      <p>This is where contact information will go for club leadership
+      so that potential contributors have a place/person to reach out to.</p>
       </div>
+      </div>
+      <br/>
       </MuiThemeProvider>
     )
   }
